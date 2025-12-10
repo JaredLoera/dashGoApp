39573e059387d66537be6ae638f7667c1d5c52b6
jared <salazarloerajared@gmail.com>
2025-12-10 12:46:43 -0600

    login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+          production: false,
+  apiUrl: 'http://localhost:3333/api',
+  storageNames: {
+    token: "token",
+    user: "user",
+
+  },
+  device: 0, // 0: Web, 1: Mobile
+  typeProfile: "1" // 0: Mobile, 1: Desktop
 };
