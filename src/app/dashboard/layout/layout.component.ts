bc10a9ccf834e7f9d5e4e2c95c22d0d90b278ddd
jared <salazarloerajared@gmail.com>
2025-12-10 13:09:46 -0600

    logout

--- src/app/dashboard/layout/layout.component.ts
+++ src/app/dashboard/layout/layout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { user } from 'src/app/core/interfaces/user';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -12,7 +14,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 export class LayoutComponent implements OnInit {
 
   userProfile: user | null = null;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.loadProfile();
@@ -23,6 +25,17 @@ export class LayoutComponent implements OnInit {
     if (userData) {
       this.userProfile = JSON.parse(userData);
     }
+  }
+  logout(): void {
+    // 1. Eliminar el token
+    localStorage.removeItem(environment.storageNames.token);
+    
+    // 2. Eliminar la información del usuario
+    localStorage.removeItem(environment.storageNames.user);
+    localStorage.removeItem(environment.typeProfile);
+    
+    // 3. Redirigir al login
+    this.router.navigate(['/']);
   }
 
 }
